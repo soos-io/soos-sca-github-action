d9cb25d4ba24d04d29a1a81fcf5a5f0dcebc50e9
SOOS-PChen <87992343+SOOS-Pchen@users.noreply.github.com>
2021-08-09 15:41:32 -0400

    soos.io to soos-io in the github address

--- entrypoint.sh
+++ entrypoint.sh
@@ -73,7 +73,7 @@ source bin/activate
 
 # Get SOOS CLI
 cd ${GITHUB_WORKSPACE}/soos/workspace
-curl -s https://api.github.com/repos/soos.io/soos-ci-analysis-python/releases/latest | grep "browser_download_url" | cut -d '"' -f 4 | xargs -n 1 curl -LO
+curl -s https://api.github.com/repos/soos-io/soos-ci-analysis-python/releases/latest | grep "browser_download_url" | cut -d '"' -f 4 | xargs -n 1 curl -LO
 sha256sum -c soos.sha256
 sha256sum -c requirements.sha256
 
